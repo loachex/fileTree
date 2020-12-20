65ee1added291270740f6b8cd8ff5dcb8718777c
LangSir <loachex@users.noreply.github.com>
2020-12-20 11:33:59 +0800

    modify:增加了结构体：folder

--- include/tree.hpp
+++ include/tree.hpp
@@ -5,7 +5,19 @@
 #define _H_DIRENT
 #endif
 
+using namespace std;
+
+typedef struct
+{
+    vector* subFolder;
+    vector* files;
+    
+    int depth;
+    int _uGSFN;//没有迭代的子文件夹数量(unGenerateSubFoldersNum)
+}folder
 
 class tree
 {
+    private:
+        
     
