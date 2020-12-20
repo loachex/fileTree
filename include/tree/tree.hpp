f443555eb4e0ed3ff7453227ecd85c0c17ff7dca
LSir <loachex@163.com>
2020-12-20 12:29:51 +0800

    modify:improve

---
+++ include/tree/tree.hpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#ifndef _H_VECTOR
+#include <vector>
+#define _H_VECTOR
+#endif
+
+#ifndef _H_DIRENT
+#include <dirent.h>
+#define _H_DIRENT
+#endif
+
+using namespace std;
+
+typedef struct
+{
+    vector<string> *subFolder;
+    vector<string> *files;
+
+    string folderName;
+    string dirPath;
+
+    int _depth; //相对于顶级文件夹的深度
+    int _uGSFN; //没有迭代的子文件夹数量(unGenerateSubFoldersNum)
+} folder;
+
+class Tree
+{
+    private:
+        folder *headFolder;
+    public:
+        int depth;
+        string headFolderPath;
+
+        Tree(string Path);
+        ~Tree();
+
+        void generate();
+};
---
+++ src/tree/tree.hpp
@@ -0,0 +1,2 @@
+#include "../../include/tree/tree.hpp"
+
