fe3675a4041ab8fc7af5d00cc51eb24c7004608f
loachex <loachex@163.com>
2020-12-31 17:24:17 +0800

    modify:完善了fileFliter类，修复了bug

--- include/tree/fliter.hpp
+++ include/tree/fliter.hpp
@@ -26,69 +26,81 @@
 
 #define LOGIC_OR 0
 #define LOGIC_AND 1
+#define LOGIC_NOT 2
 
 using namespace std;
 
+/*----------*Struct rule----------**/
 template <class T>
 struct rule
 {
     /*判断规则*/
     bool (*func)(file *f, T value); //判断函数
-    T standardValue;               //标准值
+    T standardValue;                //标准值
 };
 
+/*----------Class fileFliter----------*/
 template <class T>
 class fileFliter
 {
 private:
-    //逻辑学操作
-    bool _or;  //并
+    //逻辑值
+    bool _or;  //或
     bool _and; //与
+    bool _not; //非
 
 public:
-    /*
-map<string,rule<T>:
-判断规则字典
-    1.string:项目名称：必须是文件属性字典中的键，用于索引文件的值与标准值比较
-    2.rule<T>规则模板，包含标准值和判断函数
-*/
     map<string, rule<T>> RuleMap;
     vector<bool> results;
 
+    fileFliter()
+    {
+        //默认采用并的逻辑
+        _or = false;
+        _and = true;
+        _not = false;
+    }
     fileFliter(int logic)
     {
-        if (logic == LOGIC_OR)
-        {
-            _or = true;
-            _and = false;
-        }
-        else if (logic == LOGIC_AND)
-        {
-            _or = false;
-            _and = true;
-        }
+        //采用指定的方式
+        initLogic();
+        reviseLogic(logic);
     }
-    ~fileFliter();
+    ~fileFliter(){};
 
     //逻辑学操作
-    void reviseLogic()
+    void initLogic()
     {
-        _or = !_or;
-        _and = !_and;
-
-        result[8] = {-1};
+        _or = _and = _not = false;
+    }
+    void reviseLogic(int logic)
+    {
+        initLogic();
+        switch (logic)
+        {
+        case LOGIC_OR:
+            _or = true;
+            break;
+        case LOGIC_AND:
+            _and = true;
+            break;
+        case LOGIC_NOT:
+            _not = true;
+        default:
+            break;
+        }
     }
     bool cacuLogic(vector<bool> resultVec)
     {
+        //根据结果列表与逻辑关系，计算布尔值
         vector<bool>::iterator iter = results.begin();
+
         if (_or)
         {
             for (; iter != results.end(); iter++)
             {
                 if (*iter)
-                {
                     return true;
-                }
             }
             return false;
         }
@@ -97,11 +109,18 @@ map<string,rule<T>:
             for (; iter != results.end(); iter++)
             {
                 if (!*iter)
-                {
                     return false;
-                }
-                return true;
             }
+            return true;
+        }
+        else if (_not)
+        {
+            for (; iter != results.end(); iter++)
+            {
+                if (*iter)
+                    return false;
+            }
+            return true;
         }
         return NULL;
     }
@@ -109,20 +128,31 @@ map<string,rule<T>:
     //增加、删除规则
     void addRule(string item, rule<T> arule)
     {
+        //增加规则，项目重名时替换规则
         RuleMap[item] = arule;
+    }
+    void addRule(string item, bool (*func)(file *f, T value), T standardValue)
+    {
+        //直接输入项目名、判断函数与标准值，添加规则
+        rule<T> r{func, standardValue};
+        RuleMap[item] = r;
     }
     void delRule(string item)
     {
-        map<string, rule<T>>::iterator iter = RuleMap.find(item);
+        //根据项目删除规则
+        typename map<string, rule<T>>::iterator iter = RuleMap.find(item);
         if (iter != RuleMap.end())
             RuleMap.erase(iter);
     }
     //过滤
     bool fliter(file *f)
     {
+        //过滤：将文件利用规则字典中的每一条规则进行判断，将所有结果存入results列表中
+        //根据逻辑关系进行输出
+        results.clear();
         if (!RuleMap.empty())
         {
-            map<string, rule<T>>::iterator iter = RuleMap.begin();
+            typename map<string, rule<T>>::iterator iter = RuleMap.begin();
             for (; iter != RuleMap.end(); iter++)
             {
                 if (iter->second.func(f, iter->second.standardValue))
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -15,10 +15,28 @@ bool func(file *f, string value)
         return false;
     }
 }
+bool func2(file *f, string value)
+{
+    if (f->filePath == value)
+        return true;
+    else
+        return false;
+}
 int main()
 {
-    fileFliter ff(LOGIC_AND);
-    string item="name";
-    rule<string> r1{func,"fliter.cpp"};
-    ff.addStringRule(item,r1);
+    Tree t("/home/loachex/prog/cpp/proj/fileTree/src");
+    file *f1;
+
+    fileFliter<string> filefs1(LOGIC_NOT);
+    rule<string> r1{func, "main.cpp"};
+    rule<string> r2{func2, "/hoe/loachex/prog/cpp/proj/fileTree/src/main/main.cpp"};
+    filefs1.addRule("filename", r1);
+    filefs1.addRule("filepath", r2);
+
+    while ((f1 = t.nextFile(false)) != NULL)
+    {
+        bool result = filefs1.fliter(f1);
+        if (result)
+            cout << f1->fileName << endl;
+    }
 }
